14f05af0b078feeff4cbbb1829c21b6051a3f12b
PBL-gatech <167556129+PBL-gatech@users.noreply.github.com>
2025-05-28 10:02:55 -0400

    v21 testing
    performance gains are minimal
    
    Co-Authored-By: Benjamin Magondu <135537119+bmagondu3@users.noreply.github.com>

--- robomimic/scripts/conversion/export_to_onnx.py
+++ robomimic/scripts/conversion/export_to_onnx.py
@@ -14,7 +14,8 @@ from robomimic.utils.file_utils import policy_from_checkpoint
 
 
 # Default folder used when no CLI args are provided (pointing to repo root)
-DEFAULT_FOLDER = pathlib.Path(__file__).parent.parent.parent.parent / 'bc_patcherBot_trained_models_HEK_v0_015' / 'v0_015' / '20250507004824'
+
+DEFAULT_FOLDER = pathlib.Path(__file__).parent.parent.parent.parent / 'bc_patcherBot_trained_models_HEK_v0_021' / 'v0_021' / '20250527191420'
 
 
 def parse_args():
